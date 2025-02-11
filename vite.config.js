5546cd78256ba79e170b27b0d2d76282f9ee4a0d
ja-reggied <116132220+ja-reggied@users.noreply.github.com>
2025-02-11 11:58:33 +1100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Replace 'your-repo-name' with the actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '', // Adjust this for GitHub Pages if needed
+  base: '/PartnerExperience/'  // Set this to your GitHub repo's name
 });
