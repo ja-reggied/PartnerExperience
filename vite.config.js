3c8ff88d9cf0f7396e3c8338dca396aab4fdbd8f
ja-reggied <116132220+ja-reggied@users.noreply.github.com>
2025-02-11 12:23:30 +1100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// Replace 'your-repo-name' with the actual GitHub repo name
-export default defineConfig({
-  plugins: [react()],
-  base: '/PartnerExperience/'  // Set this to your GitHub repo's name
-});
